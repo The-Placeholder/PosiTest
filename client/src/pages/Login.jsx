500d629cce9b08fad1423d4d6487eb14dbe6e7d8
Thiem Huynh <thiem.huynh0@gmail.com>
2024-01-19 15:03:08 -0500

    daisyui and tailwind installed

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -4,13 +4,20 @@ import { Link } from 'react-router-dom';
 const Login = () => {
   return (
     <>
-      <h1>Login</h1>
+      <h1 className='text-green-500'>Login</h1>
       <p>username<input type="text" /></p>
       <p>password<input type="text" /></p>
       {/* Use the Link component to create a link to the "testerPage" route */}
       <Link to="/testerPage">
         <button id="loggingIn">Login</button>
       </Link>
+
+
+      <div>
+        <br/>
+        <p>if you can see the daisyui component below then daisyui+tailwind is properly installed</p>
+        <span className="loading loading-bars loading-lg"></span>
+      </div>
     </>
   );
 };
