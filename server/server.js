87cb9dddcca4a5f80c7a1a1eb046824c70b5a02a
Zoi <zoilanbond@gmail.com>
2024-01-23 09:30:37 -0800

    post route

--- server/server.js
+++ server/server.js
@@ -1,17 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import pg from 'pg';
+import bcrypt from 'bcrypt'
+import { createClient } from '@supabase/supabase-js'
+
 
 dotenv.config();
 
-const { Pool } = pg;
 const app = express();
+const router = express.Router()
 const port = process.env.PORT || 3000;
+const supabaseUrl = process.env.SUPABASE_URL
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL
-});
+
 
 //  ------------------------------------------------------------ MIDDLEWARE
 
@@ -19,25 +22,66 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('./public'))
 
+
+
 //  ------------------------------------------------------------ DB API ROUTES
 
-
-/* Example route to query the database */
-app.get('/instructor/:id', async (req, res) => {
-    const { id } = req.params;
-    try {
-      const client = await pool.connect();
-      const result = await client.query('SELECT * FROM user WHERE id = $1 OR assigned_to = $1', [id]);
-      const instructor = result.rows[0]; // Assuming you expect one instructor with the provided ID
-      client.release();
-      res.json(instructor);
-    } catch (err) {
-      console.error(err);
-      res.status(500).send('Error fetching instructor');
+app.get('/api/users', async (req, res) => {
+  try {
+    const { data, error } = await supabase
+    .from('user')
+    .select('*')
+    
+    if (error) {
+      console.error(error);
+      return res.status(500).send('Error fetching users');
     }
-  });
-  
+    
+    res.json(data);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error fetching users');
+  }
+});
+
+
+
+app.use('/api', router)
+router.post('/register', async (req,res) => {
+  try {
+    const {email, hashed_pw, role} = req.body
+    const {data: existingUser, error} = await supabase
+    .from('user')
+    .select('*')
+    .eq('email', email)
+
+    if(error) {
+      console.error(error)
+      return res.status(500).json({error: 'Internal Server Error'})
+    }
+
+    if(existingUser && existingUser.length > 0) {
+      return res.status(400).json({error: 'Email in use'})
+    }
+
+    const {data: newUser, registrationError} = await supabase
+    .from('user')
+    .insert([{email, hashed_pw, role}]);
+
+    if (registrationError) {
+      console.error(registrationError);
+      return res.status(500).json({ error: 'Internal Server Error during registration' });
+    }
+
+    console.log(newUser);
+    res.status(200).json({ success: 'Registration successful' });
+  } catch(err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+})
 
 app.listen(port, () => {
   console.log("Server Running on Port:", port);
 });
+
