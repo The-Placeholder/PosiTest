fe27f324190791b69e4653da53483cb50d412524
Thiem Huynh <thiem.huynh0@gmail.com>
2024-01-24 11:19:27 -0500

    created conditional state for navbar renders

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,28 @@
 import ReactDOM from 'react-dom/client';
+import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './pages/Layout';
 import Login from './pages/Login';
 import NavBar from './components/NavBar';
 import ExaminerLanding from './pages/ExaminerLanding';
+import HeroLanding from './pages/HeroLanding';
 
+/* TODO 
+  - check if new user, new user show hero?
+*/
 export default function App() {
+  const [navHide, setnavHide] = useState(false);
+
   return (
     <>
-      <NavBar />
+      {navHide ? '' : <NavBar />}
       <div id="body-ctn">
         <BrowserRouter>
           <Routes>
+            <Route
+              path="/hero"
+              element={<HeroLanding setnavHide={setnavHide} />}
+            />
             <Route path="/" element={<Layout />}>
               <Route index element={<Login />} />
             </Route>
---
+++ client/src/pages/HeroLanding.jsx
@@ -0,0 +1,7 @@
+const HeroLanding = ({ setnavHide }) => {
+  setnavHide(true);
+
+  return <h1>herolanding</h1>;
+};
+
+export default HeroLanding;
