ae6ac001457bd38598dcadab7263e89224cf4efb
Thiem Huynh <thiem.huynh0@gmail.com>
2024-01-24 18:31:31 -0500

    wireframe of testing suite complete

---
+++ client/src/pages/TestingSuite.jsx
@@ -0,0 +1,18 @@
+const TestingSuite = ({ setnavTitle }) => {
+  setnavTitle('Testing Suite');
+  return (
+    <>
+      <div className="ctn flex flex-row flex-wrap w-full h-full justify-center py-6 pb-24 gap-4">
+        <div className="ctn h-full w-3/12">
+          <h1>Code Problem Explanation</h1>
+        </div>
+        <div className="flex flex-col w-8/12 gap-4">
+          <div className="ctn w-full h-2/3">code editor</div>
+          <div className="ctn w-full h-1/3">console</div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default TestingSuite;
