cf6daf2de1917850c8c54ea8aa0387a1d9112a45
Luke Park <143543147+lukep258@users.noreply.github.com>
2024-01-26 13:59:26 -0600

    few messages

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+// delete import on implementation
 import Messenger from './components/Messenger.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    {/* move messenger component on implementation */}
     <Messenger/>
     <App />
   </React.StrictMode>,
