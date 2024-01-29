f79c0b881b1e448958bf3bd49f326114c6bd64b1
Thiem Huynh <thiem.huynh0@gmail.com>
2024-01-29 12:24:06 -0500

    fixed question hints

--- client/src/components/ProblemExplanation.jsx
+++ client/src/components/ProblemExplanation.jsx
@@ -52,63 +52,32 @@ const ProblemExplanation = ({ executeCode, code }) => {
         {questionData.instructions}
       </div>
 
-      <div id="hints" className="w-full">
-        <div className="collapse bg-base-200">
-          <input type="checkbox" />
-          <div className="collapse-title text-xl font-medium">Hints:</div>
-          <div className="collapse-content">
-            {questionData.hints}
-            <div>
-              {`What has four wings and flies? a garbage truck! i'm so cold, i can
-              use my nose drippings as chopsticks. isn't this great squidward?
-              it's just the 3 of us. you, me, and this brick wall you built
-              between us. the inner machinations of my mind are an enigma.
-              you're a man now, spongebob, and it's time you started acting like
-              one. can you give spongebob his brain back, i had to borrow it for
-              the week. did you hear about the goldfish that went bankrupted?
-              now he's a bronzefish. plankton: 1% evil, 99% hot gas. who you
-              callin' pinhead?! insurance is what i need for a crabby patty. oh
-              my god! a floating shopping list! ahhh! more soup for your arm
-              pit? soap... soap... what is soap? it's an alaskan bull worm! put
-              those eyeballs back in your head, son! the boy cries you a sweater
-              of tears... and you kill him. go out and get yourself a case of
-              the krabbies. i'll never forget your one-hundred-percent
-              all-secret patty, secretly assembled with undersea cheese,
-              pickles, lettuce, tomato, onion, all secretly steaming between two
-              fluffy seaweed sea buns. i'm cheating, mrs. puff! i'm cheating!.`}
+      {questionData.hints.items.map((hint, index) => (
+        <div
+          id={`hint${index + 1}`}
+          key={`hint${index + 1}`}
+          className="w-full"
+        >
+          <div className="collapse bg-base-200">
+            <input type="checkbox" />
+            <div className="collapse-title text-xl font-medium">
+              Hint {index}:
+            </div>
+            <div className="collapse-content">
+              {hint}
+              <div>hello</div>
             </div>
           </div>
         </div>
-      </div>
+      ))}
 
-      <div id="answer" className="w-full">
+      {/* <div id="answer" className="w-full">
         <div className="collapse bg-base-200">
           <input type="checkbox" />
           <div className="collapse-title text-xl font-medium">See Answer</div>
-          <div className="collapse-content">
-            {questionData.the_answer}
-            <div>
-              {`What has four wings and flies? a garbage truck! i'm so cold, i can
-              use my nose drippings as chopsticks. isn't this great squidward?
-              it's just the 3 of us. you, me, and this brick wall you built
-              between us. the inner machinations of my mind are an enigma.
-              you're a man now, spongebob, and it's time you started acting like
-              one. can you give spongebob his brain back, i had to borrow it for
-              the week. did you hear about the goldfish that went bankrupted?
-              now he's a bronzefish. plankton: 1% evil, 99% hot gas. who you
-              callin' pinhead?! insurance is what i need for a crabby patty. oh
-              my god! a floating shopping list! ahhh! more soup for your arm
-              pit? soap... soap... what is soap? it's an alaskan bull worm! put
-              those eyeballs back in your head, son! the boy cries you a sweater
-              of tears... and you kill him. go out and get yourself a case of
-              the krabbies. i'll never forget your one-hundred-percent
-              all-secret patty, secretly assembled with undersea cheese,
-              pickles, lettuce, tomato, onion, all secretly steaming between two
-              fluffy seaweed sea buns. i'm cheating, mrs. puff! i'm cheating!.`}
-            </div>
-          </div>
+          <div className="collapse-content">{questionData.the_answer}</div>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
