60648045f7cef8ffb23714ea61981c9493a6e8ba
guy-jerome <aaronjamesroberts@gmail.com>
2024-01-31 10:20:39 -0600

    Add the s3 bucket connection

--- aws.js
+++ aws.js
@@ -8,12 +8,23 @@ const s3 = new AWS.S3({
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 });
 
-(async () => {
-  await s3
-    .putObject({
-      Body: 'Hello World',
-      Bucket: 'nofriendo-bucket',
-      Key: 'Test.txt',
-    })
-    .promise();
-})();
+const BaseURL = 'https://nofriendo-bucket.s3.amazonaws.com/';
+const FileName = 'Test1.txt';
+async function uploadImage(BaseURL, FileName) {
+  try {
+    await s3
+      .putObject({
+        Body: 'Hello World',
+        Bucket: 'nofriendo-bucket',
+        Key: FileName,
+      })
+      .promise();
+    return `${BaseURL}${FileName}`;
+  } catch (err) {
+    return null;
+  }
+}
+
+uploadImage(BaseURL, FileName).then((response) => {
+  console.log(response);
+});
