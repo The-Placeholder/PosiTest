a71c3d4d8893a4d4adfc4dc27075415fe5c5a573
Thiem Huynh <thiem.huynh0@gmail.com>
2024-02-01 10:38:25 -0500

    added code submission landing page

--- client/src/pages/CodeSubmitPage.jsx
+++ client/src/pages/CodeSubmitPage.jsx
@@ -1,8 +1,14 @@
 const CodeSubmitPage = () => {
   return (
-    <div>
-      <h1>Congrats your code was successfully submitted</h1>
+    <div
+      id="contents-ctn"
+      className="shadow-xl flex flex-row flex-wrap justify-evenly m-5 p-5 pb-32 h-screen rounded-2xl max-h-[900px]"
+    >
+      <h1 className="text-5xl ">
+        Congrats your code was successfully submitted
+      </h1>
       <div>Your instructor will grade your test soon....?</div>
+      <div>Feel free to chat in the lobby in the mean time</div>
     </div>
   );
 };
