27f15c877f909439e6bd20bf36842eb9033f9808
Thiem Huynh <thiem.huynh0@gmail.com>
2024-02-01 09:23:59 -0500

    added navlink to root page for betterr UI

--- client/src/components/HeroLeft.jsx
+++ client/src/components/HeroLeft.jsx
@@ -1,4 +1,5 @@
 import longlogo from '/galvanize-logo-orange.png';
+import { NavLink } from 'react-router-dom';
 
 const HeroLeft = () => {
   return (
@@ -6,9 +7,12 @@ const HeroLeft = () => {
       id="hero-left"
       className="ctn w-5/12 h-full bg-g-blue flex flex-col flex-nowrap text-white content-evenly justify-center gap-8 p-20 text-center lg:p-30"
     >
-      <div className="mb-4 w-[90%] mx-auto 2xl:w-[32rem] xl:mb-16 ">
-        <img src={longlogo} alt="galvanize logo" className="w-full h-full" />
-      </div>
+      <NavLink to="/">
+        <div className="mb-4 w-[90%] mx-auto 2xl:w-[32rem] xl:mb-16 ">
+          <img src={longlogo} alt="galvanize logo" className="w-full h-full" />
+        </div>
+      </NavLink>
+
       <h1 className="text-xl font-bold lg:text-4xl">
         OUR GRADS
         <span className="text-orange-500"> CHANGE THE WORLD</span>
