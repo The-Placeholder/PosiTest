540ce55e60f30ec8c32e73a847001f63defbe5bb
Thiem Huynh <thiem.huynh0@gmail.com>
2024-01-31 22:46:02 -0500

    made hero landing more responsive

--- client/src/layouts/AccountLayout.jsx
+++ client/src/layouts/AccountLayout.jsx
@@ -8,13 +8,13 @@ export default function AccountLayout() {
       <main>
         <div
           id="hero"
-          className="fixed overflow-hidden top-0 left-0 w-full h-full min-h-screen"
+          className="fixed top-0 left-0 w-full h-full min-h-screen overflow-auto"
         >
           <div className="z-10 flex flex-row w-full h-full">
             <HeroLeft />
             <div
               id="hero-right"
-              className="ctn w-7/12 h-full bg-white text-black flex flex-row flex-wrap p-32 pt-56 gap-16 content-center"
+              className="ctn w-7/12 h-full bg-white text-black flex flex-row flex-wrap gap-10 content-center p-10 pt-56 lg:p-32 overflow-auto"
             >
               <Outlet />
             </div>
